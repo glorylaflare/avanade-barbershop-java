c4ea9e011f4e3c6f1d2b1c5eb89bef4ecae82841
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-04-02 20:39:23 -0300

    add request files

---
+++ src/main/java/br/com/dio/barbershopapi/controller/request/SaveScheduleRequest.java
@@ -0,0 +1,17 @@
+package br.com.dio.barbershopapi.controller.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.OffsetDateTime;
+
+public record SaveScheduleRequest(
+        @NotNull
+        @JsonProperty("startAt")
+        OffsetDateTime startAt,
+        @NotNull
+        @JsonProperty("endAt")
+        OffsetDateTime endAt,
+        @JsonProperty("clientId")
+        Long clientId) {
+}
---
+++ src/main/java/br/com/dio/barbershopapi/controller/request/UpdateClientRequest.java
@@ -0,0 +1,16 @@
+package br.com.dio.barbershopapi.controller.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+
+public record UpdateClientRequest(
+        @NotNull
+        @JsonProperty("name")
+        String name,
+        @NotNull
+        @JsonProperty("email")
+        String email,
+        @NotNull
+        @JsonProperty("phone")
+        String phone) {
+}

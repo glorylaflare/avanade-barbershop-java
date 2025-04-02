0f92968381bb4d1b07591cdd73d381b3a7a7462d
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-04-02 20:41:12 -0300

    fix IScheduleRepository file with the right method name

--- src/main/java/br/com/dio/barbershopapi/repository/IScheduleRepository.java
+++ src/main/java/br/com/dio/barbershopapi/repository/IScheduleRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 public interface IScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
-    List<ScheduleEntity> findByStartAtGreaterThanEqualAndEndAtLessThenEqualOrderByStartAtEndAt(final OffsetDateTime startAt, final OffsetDateTime endAt);
-    boolean existsStartAtAndEndAt(final OffsetDateTime startAt, final OffsetDateTime endAt);
+    List<ScheduleEntity> findByStartAtGreaterThanEqualAndEndAtLessThanEqualOrderByStartAtAscEndAtAsc(final OffsetDateTime startAt, final OffsetDateTime endAt);
+    boolean existsByStartAtAndEndAt(final OffsetDateTime startAt, final OffsetDateTime endAt);
 }
 

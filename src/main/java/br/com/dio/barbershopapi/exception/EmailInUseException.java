05e1d122b1ec2847c650396c370e308f1c9ff45e
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-04-02 20:39:45 -0300

    add exception files

---
+++ src/main/java/br/com/dio/barbershopapi/exception/EmailInUseException.java
@@ -0,0 +1,8 @@
+package br.com.dio.barbershopapi.exception;
+
+public class EmailInUseException extends RuntimeException {
+
+    public EmailInUseException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/br/com/dio/barbershopapi/exception/ScheduleInUseException.java
@@ -0,0 +1,8 @@
+package br.com.dio.barbershopapi.exception;
+
+public class ScheduleInUseException extends RuntimeException {
+
+    public ScheduleInUseException(String message) {
+        super(message);
+    }
+}

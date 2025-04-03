ca2190f9269a3cc4f0bc59bc138cb0cf617fe913
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-04-02 21:20:03 -0300

    att IScheduleMapper with new methods

--- src/main/java/br/com/dio/barbershopapi/mapper/IScheduleMapper.java
+++ src/main/java/br/com/dio/barbershopapi/mapper/IScheduleMapper.java
@@ -1,10 +1,14 @@
 package br.com.dio.barbershopapi.mapper;
 
 import br.com.dio.barbershopapi.controller.request.SaveScheduleRequest;
+import br.com.dio.barbershopapi.controller.response.ClientScheduleAppointmentResponse;
 import br.com.dio.barbershopapi.controller.response.SaveScheduleResponse;
+import br.com.dio.barbershopapi.controller.response.ScheduleAppointmentMonthResponse;
 import br.com.dio.barbershopapi.entity.ScheduleEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
@@ -17,4 +21,14 @@ public interface IScheduleMapper {
 
     @Mapping(target = "clientId", source = "client.id")
     SaveScheduleResponse toSaveResponse(final ScheduleEntity entity);
+
+    @Mapping(target = "scheduledAppointments", expression = "java(toClientMonthResponse(entities))")
+    ScheduleAppointmentMonthResponse toMonthResponse(final int year, final int month, final List<ScheduleEntity> entities);
+
+    List<ClientScheduleAppointmentResponse> toClientMonthResponse(final List<ScheduleEntity> entities);
+
+    @Mapping(target = "clientId", source = "client.id")
+    @Mapping(target = "clientName", source = "client.name")
+    @Mapping(target = "day", expression = "java(entity.getStartAt().getDayOfMonth())")
+    ClientScheduleAppointmentResponse toClientMonthResponse(final ScheduleEntity entity);
 }

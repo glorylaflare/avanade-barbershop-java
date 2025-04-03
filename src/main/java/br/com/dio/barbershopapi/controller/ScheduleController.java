7bdd2b0a1f3c9f2024c7c45fde8fc7cb7ec0cc28
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-04-02 21:19:36 -0300

    att ScheduleController with GetMapping

--- src/main/java/br/com/dio/barbershopapi/controller/ScheduleController.java
+++ src/main/java/br/com/dio/barbershopapi/controller/ScheduleController.java
@@ -2,6 +2,7 @@ package br.com.dio.barbershopapi.controller;
 
 import br.com.dio.barbershopapi.controller.request.SaveScheduleRequest;
 import br.com.dio.barbershopapi.controller.response.SaveScheduleResponse;
+import br.com.dio.barbershopapi.controller.response.ScheduleAppointmentMonthResponse;
 import br.com.dio.barbershopapi.entity.ScheduleEntity;
 import br.com.dio.barbershopapi.mapper.IScheduleMapper;
 import br.com.dio.barbershopapi.service.IScheduleService;
@@ -10,6 +11,9 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.YearMonth;
+
+import static java.time.ZoneOffset.UTC;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
@@ -34,5 +38,15 @@ public class ScheduleController {
     @ResponseStatus(NO_CONTENT)
     void delete(@PathVariable final long id) {
         service.delete(id);
+    }
+
+    @GetMapping("{year}/{month}")
+    ScheduleAppointmentMonthResponse listMonth(@PathVariable final int year, @PathVariable final int month) {
+        var yearMonth = YearMonth.of(year, month);
+        var startAt = yearMonth.atDay(1).atTime(0,0,0,0).atOffset(UTC);
+        var endAt = yearMonth.atEndOfMonth().atTime(23,59,59,999_999_999).atOffset(UTC);
+        var entities = queryService.findInMonth(startAt, endAt);
+        return mapper.toMonthResponse(year, month, entities);
+
     }
 }
